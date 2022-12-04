79e532b9809d7724369cb4b88038cc9f2c2ed76b
Douglas B <33129984+KryptoniteKS@users.noreply.github.com>
2022-12-04 16:59:27 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Douglas Bohning on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
