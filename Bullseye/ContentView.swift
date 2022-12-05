7c7c171fb280dd133acb447361ffc96c3c6be283
Douglas B <33129984+KryptoniteKS@users.noreply.github.com>
2022-12-05 16:48:56 -0500

    Add Basic View to App
    Adds text, a slider, and a button to our view

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        VStack
+        {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .lineSpacing(4.0)
+                .multilineTextAlignment(.center)
+                .font(.footnote)
+            Text("89")
+            
+            // Horizontal Stack for Slider and its endpoints
+            HStack
+            {
+                Text("1")
+                Slider(value: .constant(70), in: 1.00...100.00)
+                Text("100")
+            } // end HStack
+            
+            Button(action: {})
+            {
+                Text("Hit Me")
+            }
+        } // end VStack
         .padding()
     }
 }
